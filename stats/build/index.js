abbb105ee555e142221e44b99c4b52f5fb67ab66
Christian Mejia <chrismejia10@gmail.com>
2022-08-04 00:12:02 -0400

    stats: add CSV Reader; add MatchResult enum

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,16 +1,27 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
+const CsvFileReader_1 = require("./CsvFileReader");
 /**
  * Read the `football.csv`, split the file on each newline, and create nested arrays of each entry at each newline split
  */
-const matches = fs_1.default
-    .readFileSync('./data/football.csv', {
-    encoding: 'utf-8',
-})
-    .split('\n')
-    .map((row) => row.split(','));
-console.log(matches);
+const reader = new CsvFileReader_1.CSVFileReader('./data/football.csv');
+reader.read();
+/**
+ * `enum` represent all string/number possibilities. enum props can be accessed like object values via dot-notation. They cannot be edited externally (via network request, etc.) They need to be defined at code time.
+ */
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let lfcWins = 0;
+for (let match of reader.data) {
+    if (match[1] === 'Liverpool' && match[5] === MatchResult.HomeWin) {
+        lfcWins++;
+    }
+    else if (match[2] === 'Liverpool' && match[5] === MatchResult.AwayWin) {
+        lfcWins++;
+    }
+}
+console.log(`Liverpool won ${lfcWins} games this season`);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,28 @@
-import fs from 'fs';
+import { CSVFileReader } from './CsvFileReader';
 
 /**
  * Read the `football.csv`, split the file on each newline, and create nested arrays of each entry at each newline split
  */
-const matches = fs
-  .readFileSync('./data/football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
 
-console.log(matches);
+const reader = new CSVFileReader('./data/football.csv');
+reader.read();
+
+/**
+ * `enum` represent all string/number possibilities. enum props can be accessed like object values via dot-notation. They cannot be edited externally (via network request, etc.) They need to be defined at code time.
+ */
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let lfcWins = 0;
+for (let match of reader.data) {
+  if (match[1] === 'Liverpool' && match[5] === MatchResult.HomeWin) {
+    lfcWins++;
+  } else if (match[2] === 'Liverpool' && match[5] === MatchResult.AwayWin) {
+    lfcWins++;
+  }
+}
+
+console.log(`Liverpool won ${lfcWins} games this season`);
